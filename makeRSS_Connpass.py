17697376fb6e574f68aa35525844d25525a3a62c
kh <127534515+kh5402@users.noreply.github.com>
2023-10-17 19:33:00 +0900

    Update makeRSS_Connpass.py

--- makeRSS_Connpass.py
+++ makeRSS_Connpass.py
@@ -5,7 +5,6 @@ from xml.dom import minidom
 import os
 
 def main():
-    
     event_pattern = re.compile(r'<div class="recent_event_list">([\s\S]*?)<\/div>\s*<\/div>')
     
     output_file = "makeRSS_Connpass.xml"
@@ -15,7 +14,6 @@ def main():
 
     print(f"Starting with base URL: {base_url}")
 
-    # 既存のRSSフィードを読み込む
     existing_links = set()
     if os.path.exists(output_file):
         tree = ET.parse(output_file)
@@ -31,68 +29,49 @@ def main():
         ET.SubElement(channel, "description").text = description
         ET.SubElement(channel, "link").text = "https://example.com"
 
-    # 30ページまでスクレイピング
     for page in range(1, 31):
         print(f"Fetching page {page}...")
-        
-        #response = requests.get(url)
-        
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(url, headers=headers)
-        
         html_content = response.text
         print(f"Response status code: {response.status_code}")
-
         
-        events_found = 0  # このページで見つかったイベントの数 <- この行を追加
-    
-        for match in event_pattern.findall(html_content):
-            events_found += 1  # この行も追加
-            event_html = match
-
-        
+        events_found = 0
         channel = root.find("channel")
         
-
         found_events = event_pattern.findall(html_content)
         print(f"Found {len(found_events)} events on page {page}.")
         
         for match in found_events:
             event_html = match
-
             date = re.search(r'title="(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z)"', event_html).group(1)
             title_link = re.search(r'<a class="image_link event_thumb" href="(https:\/\/[a-zA-Z0-9\-\.\/]+)" title="(.*?)">', event_html)
             link, title = title_link.groups()
-
-            # 既存のリンクならスキップ
+            
             if link in existing_links:
                 continue
-
-            # 含めたいワードが含まれているかチェック
+            
             if any(word in title for word in include_words):
                 new_item = ET.SubElement(channel, "item")
                 ET.SubElement(new_item, "title").text = title
                 ET.SubElement(new_item, "link").text = link
                 ET.SubElement(new_item, "pubDate").text = date
 
-        print(f"Found {events_found} events on page {page}.")  # このページで何件見つかったか出力 <- この行も追加
+        print(f"Found {events_found} events on page {page}.")
         
-        # 次のページへ
         next_page = re.search(r'<li class="to_next"><a href="\?page=(\d+)">次へ&gt;&gt;<\/a><\/li>', html_content)
         if next_page:
             url = base_url + "?page=" + next_page.group(1)
         else:
             print("No more pages found.")
-            break  # 次のページがなければ終了
+            break
 
-        xml_str = ET.tostring(root)  # この行をmain関数の中に移動
-        xml_pretty_str = minidom.parseString(xml_str).toprettyxml(indent="  ")
-        xml_pretty_str = os.linesep.join([s for s in xml_pretty_str.splitlines() if s.strip()])  # 行間詰める処理
-
-        with open(output_file, "w") as f:
-            f.write(xml_pretty_str)
+    xml_str = ET.tostring(root)
+    xml_pretty_str = minidom.parseString(xml_str).toprettyxml(indent="  ")
+    xml_pretty_str = os.linesep.join([s for s in xml_pretty_str.splitlines() if s.strip()])
     
+    with open(output_file, "w") as f:
+        f.write(xml_pretty_str)
+
 if __name__ == "__main__":
     main()
-
-
